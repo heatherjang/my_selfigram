9225190cc11106f7cdd08a5c189ef337f263bc33
Heather Jang <heather.m.jang@gmail.com>
2016-08-01 19:13:30 -0700

    Initial Commit

---
+++ my_selfigram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my_selfigram
+//
+//  Created by Heather on 2016-08-01.
+//  Copyright © 2016 Heather. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
